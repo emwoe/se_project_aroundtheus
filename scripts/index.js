ead8436b3c896f8b5859e5a798ed0bd83cb49c46
Emma Woelk <emma.woelk@gmail.com>
2024-05-06 17:41:07 -0500

    Fix Array

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,31 @@
 let initialCards = [
-    const card1 = {
+   {
         name: "Austin",
         link: URL(https://unsplash.com/photos/river-near-buildings-during-daytime-wKfTNWaDYgs)
     }
 
     },
-    const card2 = {
+    {
         name: "New York",
         link: URL(https://unsplash.com/photos/cars-parked-near-brown-building-wOj5odhDOZ0)
     },
 
-    const card3 = {
+    {
         name: "San Francisco",
         link: URL(https://unsplash.com/photos/san-franciscos-downhill-winding-street-AFlG5jpMvYg)
     },
 
-    const card4 = {
+    {
         name: "Kansas City",
         link: URL(https://unsplash.com/photos/a-beautiful-shot-of-union-station-and-sky-scrappers-against-dusk-sky-in-kansas-city-missouri-united-states-1mOYmtdufmo)
     },
 
-    const card5 = {
-        name: "Chicago",
-        link: URL(https://unsplash.com/photos/photo-of-high-rise-building-tnv84LOjes4)
-    },
-
-    const card6 = {
-        name: "Atlanta",
-        link: URL(https://unsplash.com/photos/a-vertical-drone-view-of-the-downtown-atlanta-with-modern-buildings-and-a-large-green-park-georgia-AfnosnmGhtA)
-    }
+{
+    name: "Chicago",
+    link: URL(https://unsplash.com/photos/photo-of-high-rise-building-tnv84LOjes4)
+},
+ {
+    name: "Atlanta",
+    link: URL(https://unsplash.com/photos/a-vertical-drone-view-of-the-downtown-atlanta-with-modern-buildings-and-a-large-green-park-georgia-AfnosnmGhtA)
+ }
 ];
