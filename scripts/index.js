408b96f5c38feb12a3a09ab2c34fbcca1450768f
Emma Woelk <emma.woelk@gmail.com>
2024-05-06 18:01:36 -0500

    Fix ArrayTEST2

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,27 @@
-let initialCards = [
-    {
-        name: "Austin",
-        link: URL(https://unsplash.com/photos/river-near-buildings-during-daytime-wKfTNWaDYgs),
-       },
-   {"name": "Austin",
-"link": URL(https://unsplash.com/photos/river-near-buildings-during-daytime-wKfTNWaDYgs)
-    },
-    {
-        "name": "New York",
-        "link": URL(https://unsplash.com/photos/cars-parked-near-brown-building-wOj5odhDOZ0)
-    },
-    {
-        "name": "San Francisco",
-        "link": URL(https://unsplash.com/photos/san-franciscos-downhill-winding-street-AFlG5jpMvYg)
-    },
+const initialCards = [
+  {
+    name: "Austin",
+    link: "https://unsplash.com/photos/river-near-buildings-during-daytime-wKfTNWaDYgs",
+  },
+  {
+    name: "New York",
+    link: "https://unsplash.com/photos/cars-parked-near-brown-building-wOj5odhDOZ0",
+  },
+  {
+    name: "San Francisco",
+    link: "https://unsplash.com/photos/san-franciscos-downhill-winding-street-AFlG5jpMvYg",
+  },
 
-    {
-        "name": "Kansas City",
-        "link": URL(https://unsplash.com/photos/a-beautiful-shot-of-union-station-and-sky-scrappers-against-dusk-sky-in-kansas-city-missouri-united-states-1mOYmtdufmo)
-    },
-{
-    "name": "Chicago",
-    "link": URL(https://unsplash.com/photos/photo-of-high-rise-building-tnv84LOjes4)
-},
- {
-    "name": "Atlanta",
-    "link": URL(https://unsplash.com/photos/a-vertical-drone-view-of-the-downtown-atlanta-with-modern-buildings-and-a-large-green-park-georgia-AfnosnmGhtA)
- }
+  {
+    name: "Kansas City",
+    link: "https://unsplash.com/photos/a-beautiful-shot-of-union-station-and-sky-scrappers-against-dusk-sky-in-kansas-city-missouri-united-states-1mOYmtdufmo",
+  },
+  {
+    name: "Chicago",
+    link: "https://unsplash.com/photos/photo-of-high-rise-building-tnv84LOjes4",
+  },
+  {
+    name: "Atlanta",
+    link: "https://unsplash.com/photos/a-vertical-drone-view-of-the-downtown-atlanta-with-modern-buildings-and-a-large-green-park-georgia-AfnosnmGhtA",
+  },
 ];
