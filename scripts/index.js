05f0336b9c1502d771b5068da7774e80ffad4a53
Emma Woelk <emma.woelk@gmail.com>
2024-05-06 17:43:28 -0500

    Fix Array2

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,30 @@
 let initialCards = [
    {
-        name: "Austin",
-        link: URL(https://unsplash.com/photos/river-near-buildings-during-daytime-wKfTNWaDYgs)
+        "name": "Austin",
+        "link": URL(https://unsplash.com/photos/river-near-buildings-during-daytime-wKfTNWaDYgs)
     }
 
     },
     {
-        name: "New York",
-        link: URL(https://unsplash.com/photos/cars-parked-near-brown-building-wOj5odhDOZ0)
+        "name": "New York",
+        "link": URL(https://unsplash.com/photos/cars-parked-near-brown-building-wOj5odhDOZ0)
     },
 
     {
-        name: "San Francisco",
-        link: URL(https://unsplash.com/photos/san-franciscos-downhill-winding-street-AFlG5jpMvYg)
+        "name": "San Francisco",
+        "link": URL(https://unsplash.com/photos/san-franciscos-downhill-winding-street-AFlG5jpMvYg)
     },
 
     {
-        name: "Kansas City",
-        link: URL(https://unsplash.com/photos/a-beautiful-shot-of-union-station-and-sky-scrappers-against-dusk-sky-in-kansas-city-missouri-united-states-1mOYmtdufmo)
+        "name": "Kansas City",
+        "link": URL(https://unsplash.com/photos/a-beautiful-shot-of-union-station-and-sky-scrappers-against-dusk-sky-in-kansas-city-missouri-united-states-1mOYmtdufmo)
     },
-
 {
-    name: "Chicago",
-    link: URL(https://unsplash.com/photos/photo-of-high-rise-building-tnv84LOjes4)
+    "name": "Chicago",
+    "link": URL(https://unsplash.com/photos/photo-of-high-rise-building-tnv84LOjes4)
 },
  {
-    name: "Atlanta",
-    link: URL(https://unsplash.com/photos/a-vertical-drone-view-of-the-downtown-atlanta-with-modern-buildings-and-a-large-green-park-georgia-AfnosnmGhtA)
+    "name": "Atlanta",
+    "link": URL(https://unsplash.com/photos/a-vertical-drone-view-of-the-downtown-atlanta-with-modern-buildings-and-a-large-green-park-georgia-AfnosnmGhtA)
  }
 ];
