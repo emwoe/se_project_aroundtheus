ac3890efdbbfad1cc394be223921aca415765dd0
Emma Woelk <emma.woelk@gmail.com>
2024-05-12 20:20:02 -0500

    S4 Part 4 initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,78 @@
 const initialCards = [
   {
     name: "Austin",
-    link: "https://unsplash.com/photos/river-near-buildings-during-daytime-wKfTNWaDYgs",
+    link: "../images/card-images/Austin.jpg",
   },
   {
     name: "New York",
-    link: "https://unsplash.com/photos/cars-parked-near-brown-building-wOj5odhDOZ0",
+    link: "../images/card-images/NewYork.jpg",
   },
   {
     name: "San Francisco",
-    link: "https://unsplash.com/photos/san-franciscos-downhill-winding-street-AFlG5jpMvYg",
+    link: "../images/card-images/SanFran.jpg",
   },
 
   {
     name: "Kansas City",
-    link: "https://unsplash.com/photos/a-beautiful-shot-of-union-station-and-sky-scrappers-against-dusk-sky-in-kansas-city-missouri-united-states-1mOYmtdufmo",
+    link: "../images/card-images/KansasCity.jpg",
   },
   {
     name: "Chicago",
-    link: "https://unsplash.com/photos/photo-of-high-rise-building-tnv84LOjes4",
+    link: "../images/card-images/Chicago.jpg",
   },
   {
     name: "Atlanta",
-    link: "https://unsplash.com/photos/a-vertical-drone-view-of-the-downtown-atlanta-with-modern-buildings-and-a-large-green-park-georgia-AfnosnmGhtA",
+    link: "../images/card-images/Atlanta.jpg",
   },
 ];
 
-let modalBox = document.querySelector(".modal");
-let modalEditBtn = document.querySelector(".info__button");
-let modalCloseBtn = document.querySelector(".modal__close-button");
+let cardArea = document.querySelector(".elements");
+let cardTemplate = document.querySelector("#localeCard").content;
+
+function getCardElement() {
+  for (i = 0; i < initialCards.length; i++) {
+    let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    let cardImageLink = initialCards[i].link;
+    let cardName = initialCards[i].name;
+    cardElement.querySelector(".card__name").textContent = cardName;
+    cardElement.querySelector(".card__image").src = cardImageLink;
+    cardElement.querySelector(".card__image").alt = cardName;
+    cardArea.append(cardElement);
+  }
+}
+getCardElement();
+
+const modalBox = document.querySelector(".modal");
+const modalEditBtn = document.querySelector(".info__button");
+const modalCloseBtn = document.querySelector(".modal__close-button");
+const modalForm = document.querySelector(".modal__container");
+
+const profileName = document.querySelector(".info__name");
+const modalName = document.querySelector(".modal__name");
+const profileJob = document.querySelector(".info__job-title");
+const modalJob = document.querySelector(".modal__job");
 
 function modalOpen() {
   modalBox.classList.remove("modal");
   modalBox.classList.add("modal_opened");
+  modalName.value = profileName.textContent;
+  modalJob.value = profileJob.textContent;
 }
 
-function modalClose() {
+function modalClose(evt) {
+  evt.preventDefault();
+  modalBox.classList.add("modal");
+  modalBox.classList.remove("modal_opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = modalName.value;
+  profileJob.textContent = modalJob.value;
   modalBox.classList.add("modal");
   modalBox.classList.remove("modal_opened");
 }
 
 modalEditBtn.addEventListener("click", modalOpen);
 modalCloseBtn.addEventListener("click", modalClose);
+modalForm.addEventListener("submit", handleProfileFormSubmit);
